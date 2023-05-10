d13e1a114f99f45410313c01bfa7cd4d93480129
user <frank.sossi@gmail.com>
2023-05-10 14:27:03 -0700

    Added drag and drop UI test to see if usage it what we wanted

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import axios from 'axios';
 import { stats } from './components/Stats';
 
 function App() {
-    const [userInput, setUserInput] = useState('');
-    const [message, setMessage] = useState('');
     const [clue, setClue] = useState(null);
+    const [message, setMessage] = useState("");
+    const [letters, setLetters] = useState([]);
+    const [boxes, setBoxes] = useState([]);
 
     useEffect(() => {
         fetch('/api/clues/random')
@@ -17,53 +17,94 @@ function App() {
                 return response.json();
             })
             .then((data) => {
-                console.log('Fetched data:', data);
                 setClue(data);
+                const answerLetters = data.answer.split('');
+                const shuffledLetters = shuffle(answerLetters.map((letter, index) => ({ id: index, value: letter })));
+                setLetters(shuffledLetters);
+                setBoxes(new Array(answerLetters.length).fill(null));
             })
             .catch((error) => {
-                console.error('Error fetching random unused clue:', error);
+                console.error('Error:', error);
             });
     }, []);
 
-
-
     const handleSubmit = async () => {
-        try {
-            const response = await axios.post('http://localhost:5000/api/clues/validate', {
-
-                answer: userInput,
-                clueId: clue._id,
-            });
-
-            if (response.data.correct) {
-                stats(true)
-                setMessage('Correct! You solved the anagram.');
-            } else {
-                setMessage('Incorrect! Please try again.');
-            }
-        } catch (error) {
-            setMessage('An error occurred. Please try again.');
+        const response = await axios.post('http://localhost:5000/api/clues/validate', {
+            answer: boxes.map(box => box?.value).join(''),
+            clueId: clue._id,
+        });
+        if (response.data.correct) {
+            stats(true)
+            setMessage('Correct! You solved the anagram.');
+        } else {
+            setMessage('Incorrect! Please try again.');
         }
     };
 
+    const handleDrop = (e, index) => {
+        const id = e.dataTransfer.getData("text");
+        const letter = letters.find(item => item.id === Number(id));
+        setLetters(prevLetters => prevLetters.filter(item => item.id !== Number(id)));
+        setBoxes(prevBoxes => {
+            const newBoxes = [...prevBoxes];
+            newBoxes[index] = letter;
+            return newBoxes;
+        });
+    };
 
+    const handleDragStart = (e, id) => {
+        e.dataTransfer.setData("text", id);
+    };
+
+    const shuffle = (array) => {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
 
     return (
-        <div className="App">
+        <div className="App" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
             <h1>Anagram Game</h1>
             {clue ? (
                 <>
                     <p>Clue: {clue.clue}</p>
-                    <p>
-                        <a href={clue.articleUrl} target="_blank" rel="noopener noreferrer">
-                            View article
-                        </a>
-                    </p>
-                    <input
-                        type="text"
-                        value={userInput}
-                        onChange={(e) => setUserInput(e.target.value)}
-                    />
+                    <div style={{ display: "flex", justifyContent: "center", marginBottom: "10px" }}>
+                        {boxes.map((box, index) => (
+                            <div
+                                key={index}
+                                onDrop={e => handleDrop(e, index)}
+                                onDragOver={e => e.preventDefault()}
+                                style={{ border: "1px solid black", width: "30px", height: "30px", display: "inline-block", marginRight: "10px" }}
+                            >
+                                {box?.value}
+                            </div>
+                        ))}
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "center", marginBottom: "10px" }}>
+                        {letters.map((letter, index) => (
+                            <div
+                                key={index}
+                                draggable
+                                onDragStart={e => handleDragStart(e, letter.id)}
+                                style={{ border: "1px solid black", width: "30px", height: "30px", display: "inline-block", marginRight: "10px" }}
+                            >
+                                {letter.value}
+                            </div>
+                        ))}
+                    </div>
                     <button onClick={handleSubmit}>Submit</button>
                 </>
             ) : (
@@ -75,5 +116,7 @@ function App() {
 }
 
 export default App;
+
+
 
 
